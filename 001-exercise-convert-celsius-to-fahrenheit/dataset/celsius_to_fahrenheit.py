16dd6bb3224a2276cf2cd8179627bffc00c91203
AlexT.H <zelechos@hotmail.com>
2022-12-21 21:39:09 -0400

    feat: add script to generate a dataset in format .csv

---
+++ 001-exercise-convert-celsius-to-fahrenheit/dataset/celsius_to_fahrenheit.py
@@ -0,0 +1,9 @@
+"""Script to create a dataset of the model"""
+
+
+def celsius_to_fahrenheit(celsius):
+    """function to convert celsius to fahrenheit"""
+    print(celsius, ", ", (celsius * 9 / 5) + 32)
+
+
+celsius_to_fahrenheit(10)
